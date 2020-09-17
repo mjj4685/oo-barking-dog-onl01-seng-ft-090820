dc86b1f2c3b7185dd215b723c34a0e46e2d3a919
MJ Johnson <mjohnson4685@gmail.com>
2020-09-17 22:43:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,23 @@
 class Dog
- 
+
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
- 
+
   def name
     @this_dogs_name
   end
 end
- 
+
 lassie = Dog.new
 lassie.name = "Lassie"
- 
+
 puts lassie.name
 
 fido = Dog.new
 fido.name = "Fido"
- 
+
 fido.name
 # => "Fido"
- 
+
 fido.bark
